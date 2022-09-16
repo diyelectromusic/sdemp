3bf94a7396510c6bcf502848ef71b991d7ca2cd5
Kevin <68612569+diyelectromusic@users.noreply.github.com>
2022-09-16 21:00:06 +0100

    Update ArduinoMIDIRE7SegmentController2.ino
    Added "bank selection" mode either via RE switch or additional switch.

--- src/SDEMP/ArduinoMIDIRE7SegmentController2/ArduinoMIDIRE7SegmentController2.ino
+++ src/SDEMP/ArduinoMIDIRE7SegmentController2/ArduinoMIDIRE7SegmentController2.ino
@@ -43,6 +43,35 @@
 // be instant, but would prefer them to require a button press.
 #define INSTANT_PC 1
 
+// Comment this out if you don't want a "bank select" mode.
+// Set this to the rotary encoder switch if you want to use that
+// to switch modes.
+#define BANK_SEL 1
+
+// Optional: Use an additional switch as the bank select mode switch.
+// If not defined but BANK_SEL above is, then it is assumed the
+// rotary encoder switch changes modes (this only really makes sense
+// if the code is running in INSTANT_PC mode though!)
+//#define BANK_SEL_PIN A5
+
+// Define the MIDI CC messages to be used for the different banks
+#define NUM_BANKS 8
+int ccs[NUM_BANKS][2] = {
+  // MSB, LSB
+  {    0,   0},
+  {    0,   1},
+  {    0,   2},
+  {    0,   3},
+  {    0,   4},
+  {    0,   5},
+  {    0,   6},
+  {    0,   7},
+};
+
+// Bank Select related MIDI Control Change message values
+#define BANKSELMSB 0
+#define BANKSELLSB 32
+
 // This is required to set up the MIDI library.
 // The default MIDI setup uses the Arduino built-in serial port
 // which is pin 1 for transmitting on the Arduino Uno.
@@ -89,8 +118,12 @@ byte segmentPins[NUM_SEGMENTS] = {5,6,7,8,9,10,11}; // Order: A,B,C,D,E,F,G
 #define NO_DECIMAL_POINT      true           // Set true if no DP or not connected
 
 int prognum;
+int banknum;
 int last_sw;
 int sw_cnt;
+int last_bank;
+int bank_cnt;
+int mode;
 
 // ------------------------------------------------------
 //   The display needs to be updated continually, which
@@ -115,6 +148,10 @@ void setup() {
   // Initialise the encoder switch
   pinMode(RE_SW, INPUT_PULLUP);
 
+#ifdef BANK_SEL_PIN
+  pinMode(BANK_SEL_PIN, INPUT_PULLUP);
+#endif
+
   // Initialise the 7-Segment display
   disp.begin(HARDWARE_CONFIG, NUM_DIGITS, digitPins, segmentPins,
              RESISTORS_ON_SEGMENTS, UPDATE_WITH_DELAYS, LEADING_ZEROS, NO_DECIMAL_POINT);
@@ -127,7 +164,9 @@ void setup() {
   MIDI.begin(MIDI_CHANNEL_OMNI);
 #endif
   prognum = 0;
-  updateDisplay(prognum);
+  banknum = 0;
+  mode = 0;  // Program
+  updateProgDisplay(prognum);
 }
 
 int re_pos = 0;
@@ -148,19 +187,40 @@ void loop() {
   if (new_pos != re_pos) {
     int re_dir = (int)encoder.getDirection();
     if (re_dir < 0) {
-      prognum--;
-      prognum &= 0x7F;
+      if (mode == 0) { // Program mode
+        prognum--;
+        prognum &= 0x7F;
+      } else { // Bank mode
+        banknum--;
+        if (banknum < 0) {
+          banknum = NUM_BANKS-1;
+        }
+      }
     } else if (re_dir > 0) {
-      prognum++;
-      prognum &= 0x7F;
+      if (mode == 0) { // Program mode
+        prognum++;
+        prognum &= 0x7F;
+      } else { // Bank mode
+        banknum++;
+        if (banknum >= NUM_BANKS) {
+          banknum = 0;
+        }
+      }
     } else {
       // if re_dir == 0; do nothing
     }
     re_pos = new_pos;
-    updateDisplay(prognum);
+    if (mode == 0) { // Program mode
+      updateProgDisplay(prognum);
 #ifdef INSTANT_PC
-    updateProgNum (prognum);
+      updateProgNum (prognum);
 #endif
+    } else { // Bank mode
+      updateBankDisplay(banknum);
+#ifdef INSTANT_PC
+      updateBankNum (banknum);
+#endif
+    }
   }
 
   // Check the encoder switch with some software debouncing
@@ -176,11 +236,69 @@ void loop() {
     // Switch is triggered!
     sw_cnt = 0;
     last_sw = LOW;
-    updateProgNum (prognum);
+#ifndef INSTANT_PC
+    if (mode == 0) { // Program mode
+      updateProgNum (prognum);
+    } else { // Bank mode
+      updateBankNum (banknum);
+    }
+#else // In "instant mode" so see if we are supporting BANK selection
+#ifdef BANK_SEL
+#ifndef BANK_SEL_PIN
+    // This only makes sense if:
+    //   In INSTANT_PC mode
+    //   AND In BANK_SEL mode
+    //   AND NOT In BANK_SEL_PIN mode
+    // So if all this applies, then toggle the mode!
+    mode = !mode;
+    if (mode == 0) { // Program mode
+#ifdef TEST
+      Serial.println ("Program Mode via RE");
+#endif
+      updateProgDisplay(prognum);
+    } else { // Bank mode
+#ifdef TEST
+      Serial.println ("Bank Mode via RE");
+#endif
+      updateBankDisplay(banknum);
+    }
+#endif
+#endif
+#endif
   }
+
+#ifdef BANK_SEL_PIN
+  int bank_sw = digitalRead(BANK_SEL_PIN);
+  if (bank_sw == HIGH) {
+    // No switch pressed yet
+    bank_cnt = 0;
+    last_bank = HIGH;
+  } else {
+    bank_cnt++;
+  }
+  if ((last_bank==HIGH) && (bank_cnt >= 1000)) {
+    // Switch is triggered!
+    bank_cnt = 0;
+    last_bank = LOW;
+
+    // Toggle the mode!
+    mode = !mode;
+    if (mode == 0) { // Program mode
+#ifdef TEST
+      Serial.println ("Program Mode via SW");
+#endif
+      updateProgDisplay(prognum);
+    } else { // Bank mode
+#ifdef TEST
+      Serial.println ("Bank Mode via SW");
+#endif
+      updateBankDisplay(banknum);
+    }
+  }
+#endif
 }
 
-void updateDisplay (int progNum) {
+void updateProgDisplay (int progNum) {
   // Change to user-friendly 1 to 128 range
   disp.setNumber(progNum+1,0);
 }
@@ -193,5 +311,23 @@ void updateProgNum (int progNum) {
   // MIDI programs are defined as 1 to 128, but "on the wire"
   // have to be converted to 0 to 127.
   MIDI.sendProgramChange (progNum, MIDI_CHANNEL);
+#endif
+}
+
+void updateBankDisplay (int bankNum) {
+  // Use negative to show we're in "bank" mode
+  // Also show 1 to MAX index on the display
+  disp.setNumber(-(bankNum+1),0);
+}
+
+void updateBankNum (int bankNum) {
+#ifdef TEST
+  Serial.print ("Bank Change: ");
+  Serial.print (ccs[bankNum][0]);
+  Serial.print (",");
+  Serial.println (ccs[bankNum][1]);
+#else
+  MIDI.sendControlChange (BANKSELMSB, ccs[bankNum][0], MIDI_CHANNEL);
+  MIDI.sendControlChange (BANKSELLSB, ccs[bankNum][1], MIDI_CHANNEL);
 #endif
 }
